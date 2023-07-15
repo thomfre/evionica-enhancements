78841307275cbd7af69b5cae08e0dcbf52781790
thomfre <10846906+thomfre@users.noreply.github.com>
2023-07-15 10:06:51 +0200

    Corrected new URL

--- evionica-enhancements.userscript.meta.js
+++ evionica-enhancements.userscript.meta.js
@@ -6,7 +6,7 @@
 // @icon         https://limanovember.aero/images/icon.png
 // @author       thomfre
 // @match        https://cdn.talentlms.com/engine/*/scorm_popupV2.html
-// @match        https://cdn.talentlms.com/*/engine/*/scorm_popupV2.html
+// @match        https://cdn.talentlms.com/sc/*/engine/*/scorm_popupV2.html
 // @grant        unsafeWindow
 // @grant        GM_addStyle
 // @downloadURL  https://raw.githubusercontent.com/thomfre/evionica-enhancements/main/evionica-enhancements.userscript.js
