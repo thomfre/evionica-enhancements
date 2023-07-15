c7a03aacc6fb1ce067d66669357a9e6717157415
thomfre <10846906+thomfre@users.noreply.github.com>
2023-07-15 10:05:06 +0200

    New URL

--- evionica-enhancements.userscript.js
+++ evionica-enhancements.userscript.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         LimaNovember.Aero - Evionica CBT Enhancements
 // @namespace    https://limanovember.aero/
-// @version      0.3
+// @version      0.4
 // @description  Make the CBT a bit better for myself
 // @icon         https://limanovember.aero/images/icon.png
 // @author       thomfre
 // @match        https://cdn.talentlms.com/engine/*/scorm_popupV2.html
+// @match        https://cdn.talentlms.com/*/engine/*/scorm_popupV2.html
 // @grant        unsafeWindow
 // @grant        GM_addStyle
 // @downloadURL  https://raw.githubusercontent.com/thomfre/evionica-enhancements/main/evionica-enhancements.userscript.js
