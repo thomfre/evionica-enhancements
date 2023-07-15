f479f9a2d7b2ee1515a90e45568bd433826e359e
thomfre <10846906+thomfre@users.noreply.github.com>
2023-07-15 10:09:01 +0200

    Added another one

--- evionica-enhancements.userscript.meta.js
+++ evionica-enhancements.userscript.meta.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         LimaNovember.Aero - Evionica CBT Enhancements
 // @namespace    https://limanovember.aero/
-// @version      0.5
+// @version      0.6
 // @description  Make the CBT a bit better for myself
 // @icon         https://limanovember.aero/images/icon.png
 // @author       thomfre
 // @match        https://cdn.talentlms.com/engine/*/scorm_popupV2.html
 // @match        https://cdn.talentlms.com/sc/*/engine/*/scorm_popupV2.html
+// @match        https://cdn2.talentlms.com/sc/*/engine/*/scorm_popupV2.html
 // @grant        unsafeWindow
 // @grant        GM_addStyle
 // @downloadURL  https://raw.githubusercontent.com/thomfre/evionica-enhancements/main/evionica-enhancements.userscript.js
